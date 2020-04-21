542aa865121de875abe3ffb36261a13e129a8ee0
Michael <michael.corral@novare.com.hk>
2020-04-21 17:36:24 +0800

    Refactor payment information widget to have constructor parameters

--- lib/presentation/pages/dashboard/widgets/mobile_payment_information_widget.dart
+++ lib/presentation/pages/dashboard/widgets/mobile_payment_information_widget.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
 
 class MobilePaymentInformationWidget extends StatefulWidget {
-  MobilePaymentInformationWidget({Key key}) : super(key: key);
+  final String paymentAmountLabel;
+  final Color paymentAmountLabelColor;
+  final double paymentAmountLabelFontSize;
+
+  final String paymentAmountValue;
+  final Color paymentAmountValueColor;
+  final double paymentAmountValueFontSize;
+
+  final String dueDate;
+  final Color dueDateTextColor;
+  final double dueDateFontSize;
+
+  final String payNowButtonLabel;
+  final Color payNowButtonColor;
+  final Color payNowButtonTextColor;
+  final double payNowButtonFontSize;
+  final double payNowButtonRadius;
+  final GestureTapCallback payNowButtonOnPressed;
+
+  final String viewBillButtonLabel;
+  final Color viewBillButtonTextColor;
+  final double viewBillButtonFontSize;
+  final IconData viewBillButtonIcon;
+  final GestureTapCallback viewBillButtonOnPressed;
+
+  const MobilePaymentInformationWidget(
+      Key key,
+      this.paymentAmountLabel,
+      this.paymentAmountLabelColor,
+      this.paymentAmountLabelFontSize,
+      this.paymentAmountValue,
+      this.paymentAmountValueColor,
+      this.paymentAmountValueFontSize,
+      this.dueDate,
+      this.dueDateTextColor,
+      this.dueDateFontSize,
+      this.payNowButtonLabel,
+      this.payNowButtonColor,
+      this.payNowButtonTextColor,
+      this.payNowButtonFontSize,
+      this.payNowButtonRadius,
+      this.payNowButtonOnPressed,
+      this.viewBillButtonLabel,
+      this.viewBillButtonTextColor,
+      this.viewBillButtonFontSize,
+      this.viewBillButtonIcon,
+      this.viewBillButtonOnPressed)
+      : super(key: key);
 
   @override
   _MobilePaymentInformationWidgetState createState() =>
@@ -17,17 +64,35 @@ class _MobilePaymentInformationWidgetState
       child: Container(
         child: Column(
           children: <Widget>[
-            PaymentAmountRow(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  widget.paymentAmountLabel,
+                  style: TextStyle(
+                    fontSize: widget.paymentAmountLabelFontSize,
+                    color: widget.paymentAmountLabelColor,
+                  ),
+                ),
+                Text(
+                  widget.paymentAmountValue,
+                  style: TextStyle(
+                    fontSize: widget.paymentAmountValueFontSize,
+                    color: widget.paymentAmountValueColor,
+                  ),
+                ),
+              ],
+            ),
             SizedBox(
               height: 8,
             ),
             Row(
               children: <Widget>[
                 Text(
-                  "Due on May 13",
+                  widget.dueDate,
                   style: TextStyle(
-                    fontSize: 10.0,
-                    color: Color(0xff7F7F7F),
+                    fontSize: widget.dueDateFontSize,
+                    color: widget.dueDateTextColor,
                   ),
                 )
               ],
@@ -35,87 +100,46 @@ class _MobilePaymentInformationWidgetState
             SizedBox(
               height: 16,
             ),
-            UserActionRow(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class PaymentAmountRow extends StatefulWidget {
-  PaymentAmountRow({Key key}) : super(key: key);
-
-  @override
-  _PaymentAmountRowState createState() => _PaymentAmountRowState();
-}
-
-class _PaymentAmountRowState extends State<PaymentAmountRow> {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Text(
-          "Amount to pay",
-          style: TextStyle(
-            fontSize: 14.0,
-            color: Color(0xff019CDE),
-          ),
-        ),
-        Text(
-          "P1,798.03",
-          style: TextStyle(
-            fontSize: 14.0,
-            color: Color(0xff111111),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class UserActionRow extends StatelessWidget {
-  const UserActionRow({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: FlatButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4.0)),
-            onPressed: () {},
-            textColor: Colors.white,
-            child: Text(
-              "Pay Now",
-              style: TextStyle(fontSize: 11.0),
-            ),
-            color: Color(0xff0056BF),
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            onPressed: () {},
-            textColor: Color(0xff007DB3),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Row(
               children: <Widget>[
-                Text(
-                  "View My Bill",
-                  style: TextStyle(fontSize: 11.0),
+                Expanded(
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(widget.payNowButtonRadius)),
+                    onPressed: widget.payNowButtonOnPressed,
+                    textColor: widget.payNowButtonTextColor,
+                    child: Text(
+                      widget.payNowButtonLabel,
+                      style: TextStyle(fontSize: widget.payNowButtonFontSize),
+                    ),
+                    color: widget.payNowButtonColor,
+                  ),
                 ),
-                Icon(
-                  Icons.chevron_right,
+                Expanded(
+                  child: FlatButton(
+                    onPressed: widget.viewBillButtonOnPressed,
+                    textColor: widget.viewBillButtonTextColor,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          widget.viewBillButtonLabel,
+                          style: TextStyle(
+                              fontSize: widget.viewBillButtonFontSize),
+                        ),
+                        Icon(
+                          widget.viewBillButtonIcon,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
