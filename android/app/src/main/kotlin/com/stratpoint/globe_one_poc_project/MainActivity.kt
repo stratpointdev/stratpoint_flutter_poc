86e97a52ff32552917e86bd05ca050b9aea3a585
Akeem Romayla <aromayla@stratpoint.com>
2020-04-23 01:37:37 +0000

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/stratpoint/globe_one_poc_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/stratpoint/globe_one_poc_project/MainActivity.kt
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:android/app/src/main/kotlin/com/resocoder/finished_notes_firebase_ddd_course/MainActivity.kt
-package com.resocoder.finished_notes_firebase_ddd_course
-=======
+
 package com.stratpoint.globe_one_poc_project
->>>>>>> 4b757791a55fe7e2ad7271a82fbb24f20eb15592:android/app/src/main/kotlin/com/stratpoint/globe_one_poc_project/MainActivity.kt
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
