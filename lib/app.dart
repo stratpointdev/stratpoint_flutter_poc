68af5f151ce47b0059aa2b80161416768ebcbd7a
Zonily <zonilyjame@gmail.com>
2020-04-25 04:43:23 +0800

    Removed hex_to_int_converter.dart replaced it with native Color hex methods

--- lib/app.dart
+++ lib/app.dart
@@ -12,7 +12,6 @@ import 'package:globe_one_poc_project/infrastructure/dashboard/payment_details/r
 
 import 'application/dashboard/data_usage/data_usage_bloc.dart';
 import 'application/dashboard/account_details/account_details_bloc.dart';
-import 'common/utils/hex_to_int_converter.dart';
 import 'infrastructure/dashboard/account_details/account_details_repository_impl.dart';
 import 'infrastructure/dashboard/account_details/remote/remote_account_details_service.dart';
 import 'infrastructure/dashboard/data_usage/data_usage_repository_impl.dart';
@@ -20,7 +19,7 @@ import 'infrastructure/dashboard/data_usage/local/local_data_usage_service.dart'
 import 'infrastructure/dashboard/data_usage/remote/remote_data_usage_service.dart';
 
 import 'package:globe_one_poc_project/presentation/dashboard/mobile/dashboard_page.dart'
-if(dart.library.js) 'package:globe_one_poc_project/presentation/dashboard/web/dashboard_page.dart';
+    if (dart.library.js) 'package:globe_one_poc_project/presentation/dashboard/web/dashboard_page.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,37 +28,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     WidgetsFlutterBinding.ensureInitialized();
     return MultiBlocProvider(
-        providers: [
-          BlocProvider<PaymentDetailsBloc>(
-            create: (context) => PaymentDetailsBloc(
-                PaymentDetailsRepositoryImpl(RemotePaymentDetailsService(),
-                    LocalPaymentDetailsService())),
-          ),
-          BlocProvider<AccountDetailsBloc>(
-            create: (context) => AccountDetailsBloc(
-                AccountDetailsRepositoryImpl(RemoteAccountDetailsService())),
-          ),
-          BlocProvider<DataUsageBloc>(
-            create: (context) => DataUsageBloc(DataUsageRepositoryImpl(
-                RemoteDataUsageService(), LocalDataUsageService())),
-          ),
-        ],
-        child: MaterialApp(
-          title: 'GlobePOC',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            fontFamily: 'FSElliotPro',
-            secondaryHeaderColor: Colors.black,
-            textSelectionHandleColor:
-                Color(HexToIntConverter.convert("#007AFF")),
-            textSelectionColor: Color(HexToIntConverter.convert("#001B58")),
-            errorColor: Color(HexToIntConverter.convert("#E02020")),
-            disabledColor: Color(HexToIntConverter.convert("#007AFF")),
-            hintColor: Color(HexToIntConverter.convert("#A1A1A1")),
-            primaryColor: Color(HexToIntConverter.convert("#0056BF")),
-            accentColor: Colors.white,
-          ),
-          home: DashBoardPage(),
-        ));
+      providers: [
+        BlocProvider<PaymentDetailsBloc>(
+          create: (context) => PaymentDetailsBloc(PaymentDetailsRepositoryImpl(
+            RemotePaymentDetailsService(),
+            LocalPaymentDetailsService(),
+          )),
+        ),
+        BlocProvider<AccountDetailsBloc>(
+          create: (context) => AccountDetailsBloc(AccountDetailsRepositoryImpl(
+            RemoteAccountDetailsService(),
+          )),
+        ),
+        BlocProvider<DataUsageBloc>(
+          create: (context) => DataUsageBloc(DataUsageRepositoryImpl(
+            RemoteDataUsageService(),
+            LocalDataUsageService(),
+          )),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'GlobePOC',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'FSElliotPro',
+          secondaryHeaderColor: Colors.black,
+          textSelectionHandleColor: Color(0xff007AFF),
+          textSelectionColor: Color(0xff001B58),
+          errorColor: Color(0xffE02020),
+          disabledColor: Color(0xff007AFF),
+          hintColor: Color(0xffA1A1A1),
+          primaryColor: Color(0xff0056BF),
+          accentColor: Colors.white,
+        ),
+        home: DashBoardPage(),
+      ),
+    );
   }
 }
