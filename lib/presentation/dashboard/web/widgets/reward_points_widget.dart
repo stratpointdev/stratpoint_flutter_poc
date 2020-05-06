133fcf294077a1b1bf0eaf64bf344bf1d3066a04
Paolo Alcabao <paoloalcabao@SP-PAlcabao-MacBook-Pro-2320.local>
2020-05-07 00:41:49 +0800

    Syntax and code grouping errors.

--- lib/presentation/dashboard/web/widgets/reward_points_widget.dart
+++ lib/presentation/dashboard/web/widgets/reward_points_widget.dart
@@ -4,10 +4,9 @@ import 'package:flutter/material.dart';
 import '../../../../r.dart';
 
 class RewardPointsWidget extends StatelessWidget {
+  const RewardPointsWidget({this.points = 197, this.hasTitlePadding = false});
   final int points;
   final bool hasTitlePadding;
-
-  const RewardPointsWidget({this.points: 197, this.hasTitlePadding: false});
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +24,13 @@ class RewardPointsWidget extends StatelessWidget {
             ),
             child: Text(
               'Reward Points',
-              style: TextStyle(fontSize: 22, color: Color(0xff244857), fontWeight: FontWeight.w100),
+              style: TextStyle(fontSize: 22, color: const Color(0xff244857), fontWeight: FontWeight.w100),
               textAlign: TextAlign.left,
             ),
           ),
           Container(
             color: Colors.white,
-            padding: EdgeInsets.only(left: leftPadding, top: 24, right: 20, bottom: 20),
+            padding: const EdgeInsets.only(left: leftPadding, top: 24, right: 20, bottom: 20),
             height: hasTitlePadding ? 361 : 343,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -39,35 +38,35 @@ class RewardPointsWidget extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    Text(
+                    const Text(
                       'You Have:',
                       style: TextStyle(fontSize: 18, color: Color(0xff244857)),
                       textAlign: TextAlign.left,
                     ),
-                    SizedBox(height: 16),
-                    Spacer(),
+                    const SizedBox(height: 16),
+                    const Spacer(),
                     Text(
                       '$points ' + (points == 1 ? 'point' : 'points'),
-                      style: TextStyle(fontSize:hasTitlePadding ? 26 : 36, color: Color(0xff244857),fontWeight: FontWeight.w700),
+                      style: TextStyle(fontSize:hasTitlePadding ? 26 : 36, color: const Color(0xff244857),fontWeight: FontWeight.w700),
                       textAlign: TextAlign.left,
                     ),
                   ],
                 ),
-                SizedBox(height: 16),
-                Text(
+                const SizedBox(height: 16),
+                const Text(
                   'As our customer, you’re in for a surprise every day! from your favorite mobile promos to treats, there’s a world of wonderful rewards that awaits you!',
                   style: TextStyle(fontSize: 14, color: Color(0xff244857)),
                   textAlign: TextAlign.left,
                 ),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 SingleChildScrollView(
-                  physics: BouncingScrollPhysics(),
+                  physics: const BouncingScrollPhysics(),
                   scrollDirection: Axis.horizontal,
                   child: Row(
                     children: [R.assetsImagesImage1, R.assetsImagesImage2, R.assetsImagesImage3, R.assetsImagesImage4]
                         .map(
                           (image) => Container(
-                            padding: EdgeInsets.only(top: 16),
+                            padding: const EdgeInsets.only(top: 16),
                             width: 143,
                             height: 153,
                             decoration: BoxDecoration(
@@ -83,14 +82,14 @@ class RewardPointsWidget extends StatelessWidget {
                 ),
                 Row(
                   children: <Widget>[
-                    Spacer(),
+                    const Spacer(),
                     Padding(
-                      padding: EdgeInsets.only(top: 20),
+                      padding: const EdgeInsets.only(top: 20),
                       child: Text(
-                        "Go to Globe Rewards",
+                        'Go to Globe Rewards',
                         style: TextStyle(
                           fontSize: 14,
-                          color: Color(0xff244857),
+                          color: const Color(0xff244857),
                           decoration: TextDecoration.underline,
                           fontWeight: FontWeight.w700,
                         ),
