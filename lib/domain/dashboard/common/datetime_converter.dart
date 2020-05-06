5fad7e586e824d7925b6c84e4b616f99cf5d631b
Paolo Alcabao <paoloalcabao@SP-PAlcabao-MacBook-Pro-2320.local>
2020-05-07 02:07:37 +0800

    Fixed on datetime converter.

--- lib/domain/dashboard/common/datetime_converter.dart
+++ lib/domain/dashboard/common/datetime_converter.dart
@@ -3,29 +3,29 @@ import 'package:intl/intl.dart';
 class DateTimeConverter {
   static String getDateWithYearNow() {
     final DateTime dateTime = DateTime.now();
-    final formatter = DateFormat('MMM. dd yyyy');
+    final DateFormat formatter = DateFormat('MMM. dd yyyy');
     final String formatted = formatter.format(dateTime);
     return formatted;
   }
 
   static String convertToDate(String date) {
     final DateTime dateTime = DateTime.parse(date);
-    final formatter = DateFormat('MMM dd');
+    final DateFormat formatter = DateFormat('MMM dd');
     final String formatted = formatter.format(dateTime);
     return formatted;
   }
 
   static String convertToDateWithYear(String date) {
     final DateTime dateTime = DateTime.parse(date);
-    final formatter = DateFormat('MMM. dd yyyy');
+    final DateFormat formatter = DateFormat('MMM. dd yyyy');
     final String formatted = formatter.format(dateTime);
     return formatted;
   }
 
   static String convertToDateTime(String date) {
     final DateTime dateTime = DateTime.parse(date);
-    final dateFormatter = DateFormat('MMM. dd yyyy,');
-    final timeFormatter = DateFormat(' hh:mm aa');
+    final DateFormat dateFormatter = DateFormat('MMM. dd yyyy,');
+    final DateFormat timeFormatter = DateFormat(' hh:mm aa');
     final String formatted = dateFormatter.format(dateTime) +
         timeFormatter.format(dateTime).replaceFirst('0', '');
     return formatted;
@@ -33,13 +33,13 @@ class DateTimeConverter {
 
   static String getTimeNow() {
     final DateTime dateTime = DateTime.now();
-    final formatter = DateFormat('hh:mm aa');
+    final DateFormat formatter = DateFormat('hh:mm aa');
     final String formatted = formatter.format(dateTime).replaceFirst('0', '');
     return formatted;
   }
 
   static int getSecsDiff(String date) {
-    final lastAPICallDate = convertToComparable(date);
+    final DateTime lastAPICallDate = convertToComparable(date);
     return DateTime.now().difference(lastAPICallDate).inSeconds;
   }
 
