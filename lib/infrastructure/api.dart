51f1a5392fd4b778695475bc3b398b8eacbb84e6
Alex <aayso@stratpoint.com>
2020-05-07 00:54:00 +0800

    Fixed ../infrastructure/api.dart, api_database.dart and database_factory.dart

--- lib/infrastructure/api.dart
+++ lib/infrastructure/api.dart
@@ -1,18 +1,19 @@
 class Api {
-  String _baseUrl = 'gah-poc-flutter.firebaseio.com';
+  final String _baseUrl = 'gah-poc-flutter.firebaseio.com';
 
   Uri getSubscriberDetails() {
-    var path = "/endpoint/postpaid/v1/account/subscriber-details.json";
+    const String path = '/endpoint/postpaid/v1/account/subscriber-details.json';
     return Uri.https(_baseUrl, path);
   }
 
   Uri getOutStandingBalance() {
-    var path = "/endpoint/postpaid/v1/account/outstanding-balance.json";
+    const String path =
+        '/endpoint/postpaid/v1/account/outstanding-balance.json';
     return Uri.https(_baseUrl, path);
   }
 
   Uri getDataUsage() {
-    var path = "/endpoint/postpaid/v1/account/data-usage.json";
+    const String path = '/endpoint/postpaid/v1/account/data-usage.json';
     return Uri.https(_baseUrl, path);
   }
 
