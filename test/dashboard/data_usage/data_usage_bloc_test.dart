a501b0ef1112edffcb112dfbef44957b179d6b1b
alromayla <akeem.romayla@gmail.com>
2020-05-10 10:23:31 +0800

    -unit test fixes

--- test/dashboard/data_usage/data_usage_bloc_test.dart
+++ test/dashboard/data_usage/data_usage_bloc_test.dart
@@ -5,6 +5,7 @@ import 'package:globe_one_poc_project/application/dashboard/data_usage/data_usag
 import 'package:globe_one_poc_project/application/dashboard/data_usage/data_usage_state.dart';
 import 'package:globe_one_poc_project/domain/dashboard/data_usage/data_usage_repository.dart';
 import 'package:globe_one_poc_project/domain/dashboard/data_usage/entities/data_usage_model.dart';
+import 'package:globe_one_poc_project/domain/dashboard/data_usage/entities/data_usage_request_body.dart';
 import 'package:mockito/mockito.dart';
 
 class MockDataUsageRepository extends Mock implements DataUsageRepository {}
@@ -28,6 +29,8 @@ void main() {
         dataUsed: '',
         endDate: '',
         type: '');
+    final DataUsageRequestBody dataUsageRequestBody = DataUsageRequestBody(
+        serviceNumber: '', forceRefresh: false, primaryResourceType: '');
     final List<MainData> dataUsages = <MainData>[];
     dataUsages.add(mainData);
     final PromoSubscriptionUsage promoSubscriptionUsage =
@@ -35,7 +38,7 @@ void main() {
     final DataUsageModel dataUsageModel =
         DataUsageModel(promoSubscriptionUsage: promoSubscriptionUsage);
 
-    when(mockRepository.getDataUsage())
+    when(mockRepository.getDataUsage(dataUsageRequestBody))
         .thenAnswer((_) async => right(dataUsageModel));
 
     bloc.add(RefreshDataUsageEvent());
